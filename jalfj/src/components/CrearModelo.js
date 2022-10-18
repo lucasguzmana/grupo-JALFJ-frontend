03120fe1567300506fb26df637ea412698ad3338
lucasguzmana <lucasguzmana@uc.cl>
2022-10-18 13:02:11 -0300

    avance en vista crear modelos

--- jalfj/src/components/CrearModelo.js
+++ jalfj/src/components/CrearModelo.js
@@ -110,3 +110,58 @@
 // };
 
 // export default CrearModelo;
+
+import axios from 'axios';
+//import { useState, useEffect } from 'react';
+
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+function CrearModelo(props) {
+    
+    const NuevoModelo = async () => {
+        const nombre = document.getElementById("nombre").value;
+        const parametro1 = document.getElementById("param_1").value;
+        const parametro2 = document.getElementById("param_2").value;
+        const parametro3 = document.getElementById("param_3").value;
+        const parametro4 = document.getElementById("param_4").value;
+        const parametro5 = document.getElementById("param_5").value;
+        const url = `${SERVER_URL}/modelos/${props.user_id}`;
+        const body = {
+            nombre: nombre,
+            param_1: parametro1,
+            param_2: parametro2,
+            param_3: parametro3,
+            param_4: parametro4,
+            param_5: parametro5
+        };
+        await axios.post(url, body)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    };
+    
+    return (
+        <div>
+            <label for="standard-select">Crea un modelo:</label>
+            <label>Nombre:</label>
+            <input type="text" id="nombre" name="nombre" />
+            <label>Parametro 1:</label>
+            <input type="number" id="param_1" name="param_1" />
+            <label>Parametro 2:</label>
+            <input type="number" id="param_2" name="param_2" />
+            <label>Parametro 3:</label>
+            <input type="number" id="param_3" name="param_3" />
+            <label>Parametro 4:</label>
+            <input type="number" id="param_4" name="param_4" />
+            <label>Parametro 5:</label>
+            <input type="number" id="param_5" name="param_5" />
+            <button onClick={NuevoModelo}>Crear</button>
+        </div>
+    );
+}
+
+export default CrearModelo;
+    
